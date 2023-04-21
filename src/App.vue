3592a9135840067aba7b170011c8f8f7a5c88de6
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-04-21 14:49:00 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { Dropdown, ListGroup, ListGroupItem } from 'flowbite-vue'
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Fancy tailwind h1 header
+  </h1>
+  <p>
+    <a href="https://tailwindcss.com/docs/guides/vite#vue">Source: https://tailwindcss.com/docs/guides/vite#vue</a>
+  </p>
+  <hr>
+
+  <h1 class="mt-6">
+    Flowbite dropdown
+  </h1>
+  <dropdown text="Click me" placement="top">
+    <list-group>
+      <list-group-item>Item #1</list-group-item>
+      <list-group-item>Item #2</list-group-item>
+      <list-group-item>Item #3</list-group-item>
+    </list-group>
+  </dropdown>
+  <p>
+    <a href="https://flowbite-vue.com/pages/getting-started">Source: https://flowbite-vue.com/pages/getting-started</a>
+  </p>
+  <hr>
+
+  <h1 class="mt-6">
+    Original Vite HelloWorld component (without styles):
+  </h1>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="./assets/vite.svg" class="logo vite" alt="Vite logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
